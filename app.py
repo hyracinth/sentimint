ea8b8be984ebb234c194d1916df9049424ae079f
hyracinth <jonahlou@gmail.com>
2025-07-04 21:40:09 -0400

    Added dropdown for period and navbar pages

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
+import dash
 from dash import (
     Dash,
-    clientside_callback,
     html,
-    dash_table,
-    dcc,
-    callback,
-    Output,
-    Input,
 )
-import yfinance as yf
-import pandas as pd
-import plotly.express as px
 import dash_bootstrap_components as dbc
 
-# Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.LUMEN]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
 
 navbar = dbc.NavbarSimple(
     children=[
@@ -25,47 +16,16 @@ navbar = dbc.NavbarSimple(
     brand="SentiMint",
     brand_href="#",
     color="primary",
-    dark=True,
+    # dark=True,
 )
 
 # App layout
-app.layout = html.Div([
-    navbar,
-    dbc.Container(
-        [
-            html.H2("Ticker Tracker"),
-            dcc.Input(id="ticker-input", type="text", placeholder="Enter ticker (e.g. SPY)", debounce=True),
-            dcc.Graph(id="price-chart"),
-        ],
-        fluid=True,
-    )]
+app.layout = html.Div(
+    [
+        navbar,
+        dash.page_container,
+    ]
 )
 
-@app.callback(
-    Output("price-chart", "figure"),
-    Input("ticker-input", "value"),
-)
-def update_price_chart(ticker):
-    if not ticker:
-        return px.line(title="Enter a ticker to see the price chart")
-
-    try:
-        df = yf.Ticker(ticker.upper()).history(period="6mo")
-        if df.empty:
-            return px.line(title=f"No data found for {ticker}")
-        
-        fig = px.scatter(
-            df,
-            x=df.index,
-            y="Close",
-            title=f"{ticker.upper()} Price Chart",
-            labels={"x": "Date", "Close": "Price"},
-            # template="plotly_dark",
-        )
-    except Exception as e:
-        fig = px.line(title=f"Error fetching data for {ticker}: {str(e)}")
-
-    return fig
-# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
